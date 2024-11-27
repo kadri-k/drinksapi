41c3d6466e1ff41e46982e7a11592de38698bb11
kadri-k <kklampe@gmail.com>
2024-11-27 11:56:55 +0000

    refactor: add homecontroller class and class method

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping(value = "/")
+    public String home() {
+        return "Welcome to the Drinks API!";
+    }
 }
