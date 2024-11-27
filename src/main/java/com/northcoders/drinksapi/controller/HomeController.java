dc3932c737fb89072add3c0dd670262604c8c84c
Abulaf1a <peterrazzell688@gmail.com>
2024-11-27 12:27:23 +0000

    refactor: move controller package into com.northcoders.drinksapi package and add getCoffee method

--- src/main/java/com/northcoders/drinksapi/controller/HomeController.java
+++ src/main/java/com/northcoders/drinksapi/controller/HomeController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.northcoders.drinksapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +9,10 @@ public class HomeController {
     @GetMapping(value = "/")
     public String home() {
         return "Welcome to the Drinks API!";
+    }
+
+    @GetMapping(value = "/coffeelover")
+    public String getCoffee(){
+        return "I like coffee!";
     }
 }
